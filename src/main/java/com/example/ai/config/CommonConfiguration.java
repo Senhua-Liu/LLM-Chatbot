064f09249ea71b29e487c91f0234066059d6d785
Senhua <senhua.liu@efrei.net>
2025-09-18 09:28:15 +0800

    connect to front end

--- src/main/java/com/example/ai/config/CommonConfiguration.java
+++ src/main/java/com/example/ai/config/CommonConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ public class CommonConfiguration {
         return ChatClient
                 .builder(model)
                 .defaultSystem("You are a helpful assistant. Your name is Talon. Answer as long as possible.")
+                .defaultAdvisors(new SimpleLoggerAdvisor())
                 .build();
     }
 }
