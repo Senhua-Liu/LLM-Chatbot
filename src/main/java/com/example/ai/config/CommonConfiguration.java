8b3c1bc1f267ce3448f30fd77a41b9f4bb2a9524
Senhua <senhua.liu@efrei.net>
2025-09-24 16:02:44 +0800

    Chinese Girlfriend Simulator

--- src/main/java/com/example/ai/config/CommonConfiguration.java
+++ src/main/java/com/example/ai/config/CommonConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.ai.config;
 
+import com.example.ai.constants.SystemConstants;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
@@ -7,6 +8,7 @@ import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -31,6 +33,18 @@ public class CommonConfiguration {
         return ChatClient
                 .builder(model)
                 .defaultSystem("You are a helpful assistant. Your name is Talon. Answer as long as possible.")
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor(),
+                        MessageChatMemoryAdvisor.builder(chatMemory).build()
+                )
+                .build();
+    }
+
+    @Bean
+    public ChatClient gameChatClient(OpenAiChatModel model, ChatMemory chatMemory) {
+        return ChatClient
+                .builder(model)
+                .defaultSystem(SystemConstants.GAME_SYSTEM_PROMPT)
                 .defaultAdvisors(
                         new SimpleLoggerAdvisor(),
                         MessageChatMemoryAdvisor.builder(chatMemory).build()
